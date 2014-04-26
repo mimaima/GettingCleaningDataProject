d0dc7a50f094a36780841a492e9630589cb378b8
mimaima <mimaima@users.noreply.github.com>
2014-04-26 00:38:41 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,11 @@
 # Source of data for the project:
 # https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+# assuming that the folder "/UCI HAR Dataset" was downloaded, unzipped, and stored locally. 
+
+#  check that the current directory contains the desired data file
+if(!is.element("UCI HAR Dataset",list.files()))
+   print('Cannot find data file.')
+   
 # This R script does the following:
 # 1. Merges the training and the test sets to create one data set
 tmp1 <- read.table("train/X_train.txt")

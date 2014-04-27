00d485c591055678af6af11206559142339a5763
mimaima <mimaima@users.noreply.github.com>
2014-04-26 21:32:12 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -26,20 +26,19 @@ S <- rbind(Strn, Stst)
 Y <- rbind(Ytrn, Ytst)
 
 # 3. Extracts only the measurements on the mean and standard deviation for each measurement. 
-t_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+t_features <- grep("_Mean\\(\\)|_Std\\(\\)", features[, 2])
 X <- X[, t_features]
 names(X) <- features[t_features, 2]
 names(X) <- gsub("\\(|\\)", "", names(X))
-names(X) <- tolower(names(X)) 
+#names(X) <- tolower(names(X)) 
 
 # 4. Uses descriptive activity names to name the activities in the data set 
 #    and appropriately labels the data set with descriptive activity names.
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
 Y[,1] = activities[Y[,1], 2]
 names(Y) <- "activity"
- 
 names(S) <- "subject"
-NewData <- cbind(S, Y, X)  # Cleaned Data
+NewData <- cbind(S, Y, X)  # Got Cleaned Data
 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 uniqueSub = unique(S)[,1]
